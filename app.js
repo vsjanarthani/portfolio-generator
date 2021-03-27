ad03a02ddaea00ab84975102cc1ed9d38f3f20a3
jana <vs.janarthani@gmail.com>
2021-03-26 19:57:57 -0700

    accepting user input code added

---
+++ app.js
@@ -0,0 +1,7 @@
+const profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs)
